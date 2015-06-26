bcb3aa72b2d1a72c23aa7290211d6ee970b3af5e
NomadNick <emailnick123@gmail.com>
2015-06-25 21:16:01 -0700

    add color theme and style elements

--- js/myApp.js
+++ js/myApp.js
@@ -2,6 +2,12 @@ var app = angular.module('igSearch', []);
 
 app.controller('MyCtrl', function ($scope) {
 
+  $scope.tagSearch = function () {
+    var inputCheck = /[A-Za-z]/;
+    $scope.inputCheck = inputCheck;
+    $scope.inputCheck = true;
+    console.log('This all works here!');
+  };
 
 
 });
